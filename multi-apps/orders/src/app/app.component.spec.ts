9cdb4c044bb8ca41348bd0a92db72b0e9eb65787
Pedro Salomon <pedro.salomon@gmail.com>
2025-04-30 18:46:01 +0200

    added multi-apps domain#

--- multi-apps/orders/src/app/app.component.spec.ts
+++ multi-apps/orders/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'monolithic-app' title`, () => {
+  it(`should have the 'orders' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('monolithic-app');
+    expect(app.title).toEqual('orders');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, monolithic-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, orders');
   });
 });
