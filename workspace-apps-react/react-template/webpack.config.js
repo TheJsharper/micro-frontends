eb1c377efefe27ba0f71470d4fd54da6f60d1c52
Pedro Salomon <pedro.salomon@gmail.com>
2025-05-08 09:17:58 +0200

    formatted code

--- workspace-apps-react/react-template/webpack.config.js
+++ workspace-apps-react/react-template/webpack.config.js
@@ -2,51 +2,30 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry:  path.resolve(__dirname,  './src/index.tsx'),
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.jsx'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(ts|tsx)$/,
-                exclude: /node_modules/,
-                include: path.resolve(__dirname, './src'),
-                use: [{
-                    loader: 'babel-loader'
-                }],
-            }
-        ],
-    },
-    output: {
-        path: path.resolve(__dirname,  'build'),
-        filename: 'bundle.js',
-    },
-    mode: 'development',
-    plugins: [
-        new HtmlWebpackPlugin({
-            template:   path.resolve(__dirname,  './src/index.html')
-        }),
-    ],
-}
-/*//const path = require('path');
-
-module.exports = {
-  entry: './src/index.tsx',
+  entry: path.resolve(__dirname, './src/index.tsx'),
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
+        test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
-      },
+        include: path.resolve(__dirname, './src'),
+        use: [{
+          loader: 'babel-loader'
+        }],
+      }
     ],
   },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
   output: {
-    filename: 'bundle.js',
     path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
   },
-};*/
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html')
+    }),
+  ],
+}
