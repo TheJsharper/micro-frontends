4f1266ed55411e830f1d7d96b9d7562ccf6d2fa3
Pedro Salomon <pedro.salomon@gmail.com>
2025-05-08 09:16:57 +0200

    first commit

---
+++ workspace-apps-react/react-template/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom/client';
+export const App = () => {
+  return (
+    <div>
+      <h1>React Template</h1>
+      <p>Welcome to the React template!</p>
+    </div>
+  );
+}
