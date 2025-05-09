b40d91f8314d53599d26c5e71ff37595034d0f89
Pedro Salomon <pedro.salomon@gmail.com>
2025-05-09 18:18:29 +0200

    bootstrap added

--- workspace-apps-react/ws-mf-webpack-react/projects/host/src/App.tsx
+++ workspace-apps-react/ws-mf-webpack-react/projects/host/src/App.tsx
@@ -15,8 +15,8 @@ export const App = () => {
     <div className='app-container'>
       <div>
         <h1>React Host App  </h1>
-        <img src={logo} alt="Placeholder"  width="50" height="50"/>
-        <img src={svg} alt="Placeholder"  width="50" height="50"/>
+        <img src={logo} alt="Placeholder" width="50" height="50" />
+        <img src={svg} alt="Placeholder" width="50" height="50" />
         <p>Welcome to the React template!</p>
         <React.Suspense fallback={<div>Loading...</div>}>
           <ProductsApp />
@@ -29,14 +29,14 @@ export const App = () => {
         </React.Suspense>
 
         <div className="mb-3">
-  <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
-  <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
-</div>
-<div className="mb-3">
-  <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-  <textarea className="form-control" id="exampleFormControlTextarea1" ></textarea>
-</div>
-        
+          <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
+          <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+          <textarea className="form-control" id="exampleFormControlTextarea1" ></textarea>
+        </div>
+
       </div>
     </div>
   );
